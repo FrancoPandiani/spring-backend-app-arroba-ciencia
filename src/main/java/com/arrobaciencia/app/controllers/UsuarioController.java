dfa05ab9270b1e6111f6f77a3d458a702ada560d
Franco Pandiani <francopandiani92@gmail.com>
2022-02-20 13:48:34 -0300

    Update UsuarioController.java

--- src/main/java/com/arrobaciencia/app/controllers/UsuarioController.java
+++ src/main/java/com/arrobaciencia/app/controllers/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.arrobaciencia.app.controllers;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
