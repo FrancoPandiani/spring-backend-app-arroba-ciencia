b9183b51afc412859f0a5b44f29ae50370e15971
Franco Pandiani <francopandiani92@gmail.com>
2022-02-20 13:52:07 -0300

    Update Usuario.java

--- src/main/java/com/arrobaciencia/app/models/Usuario.java
+++ src/main/java/com/arrobaciencia/app/models/Usuario.java
@@ -1,7 +1,5 @@
 package com.arrobaciencia.app.models;
-
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,8 +14,6 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "Usuarios")
 public class Usuario implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
