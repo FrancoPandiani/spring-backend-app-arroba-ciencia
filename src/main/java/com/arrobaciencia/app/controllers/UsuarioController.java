aa389c32f5e1301f72085c946bbf44f02efaba71
Franco Pandiani <francopandiani92@gmail.com>
2022-02-20 13:49:23 -0300

    Update UsuarioController.java

--- src/main/java/com/arrobaciencia/app/controllers/UsuarioController.java
+++ src/main/java/com/arrobaciencia/app/controllers/UsuarioController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.arrobaciencia.app.models.Usuario;
 import com.arrobaciencia.app.services.UsuarioService;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+
 @RestController
 @RequestMapping("/usuarios")
 
